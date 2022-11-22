8bb97c15e7a6df33b1483e09eb8a3bbf46aedaf4
s-kakol <kakolsylwester@gmail.com>
2022-11-22 22:18:43 +0100

    Update lab-03

--- 3/test/main.test.js
+++ 3/test/main.test.js
@@ -33,36 +33,3 @@ describe('Initializing Selenium Webdriver Tests', () => {
     expect(buttons.length).toEqual(5);
   });
 });
-
-// async function github_login() {
-//
-//
-//   // await driver.get('https://the-internet.herokuapp.com/');
-//   // await driver.findElement(By.linkText('Basic Auth')).click();
-//   // await driver.wait(until.alertIsPresent());
-//   //
-//   // let alert = await driver.switchTo().alert();
-//   // await alert.sendKeys('admin');
-//
-//   // await driver.findElement(By.name('q')).sendKeys('Hello, World!', Key.RETURN);
-// }
-
-// async function addAndCountElements() {
-//   let driver = await new Builder().forBrowser('firefox').build();
-//
-//   await driver.get('https://the-internet.herokuapp.com/add_remove_elements/');
-//   let addButton = await driver.findElement(By.xpath('//*[contains(text(),\'Add Element\')]'));
-//
-//   for (let i = 0; i < 5; i++) {
-//     await addButton.click();
-//   }
-//
-//   let createdButtons = await driver.findElements(By.className('added-manually'));
-//
-//
-//   console.log(createdButtons);
-//   console.log(createdButtons.length);
-// }
-//
-// // github_login();
-// addAndCountElements();
